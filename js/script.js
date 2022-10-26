1994e81ef5f4f7971392d03bfa09615009d1b942
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-10-26 22:01:02 +0000

    Program Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Julienka Sohal All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Julienka Sohal
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
@@ -10,14 +10,23 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-Unit3-01-Storyboard/sw.js", {
+    scope: "/ICS20-Unit3-01-Storyboard/",
   })
 }
 
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculateClicked() {
+//input
+  const baseOneOfTrapezoid = parseInt(document.getElementById("base-one-of-trapezoid").value)
+  const baseTwoOfTrapezoid = parseInt(document.getElementById("base-two-of-trapezoid").value)
+  const heightOfTrapezoid = parseInt(document.getElementById("height-of-trapezoid").value)
+  
+//process
+  const areaOfTrapezoid = baseOneOfTrapezoid + baseTwoOfTrapezoid * heightOfTrapezoid / 2
+
+//output
+  document.getElementById("area").innerHTML = "The area of this Trapezoid is " + areaOfTrapezoid.toFixed(2) + " cm²."
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var GHPATH = "/ICS2O-PWA-Test"
+var GHPATH = "/ICS20-Unit3-01-Storyboard"
 var APP_PREFIX = "gppwa_"
 var VERSION = "version_001"
 var URLS = [
